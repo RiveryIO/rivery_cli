c2ff134e5d4ee47067bc280b1b620ae68553846d
Alon Reznik <alonreznik@gmail.com>
2021-03-11 15:27:59 +0200

    :sparkles:?feat(cli.init.py): Create project initiation with 'rivery init'

--- rivery_cli/cli/configure.py
+++ rivery_cli/cli/configure.py
@@ -18,6 +18,10 @@ def create_auth_file(ctx, **kwargs):
     region = ctx.get('REGION')
     host = ctx.get('HOST')
 
+    # Create ~/.rivery if not exists
+    if not pathlib.Path(HOME_DIR).exists():
+        pathlib.Path(HOME_DIR).mkdir(exist_ok=True)
+    # Create ~/.rivery/auth if not exists
     auth_path = pathlib.Path(BASE_AUTH_PATH)
     if not auth_path.exists():
         auth_path.touch()
@@ -25,25 +29,28 @@ def create_auth_file(ctx, **kwargs):
     with open(auth_path, 'r') as af:
         auth_config = yaml.safe_load(af) or {}
 
+    # Check the config and write it if not exists
     profile_auth = auth_config.get(profile) or {}
     token = auth_config.get('token') or ''
     host = host or auth_config.get('host')
     if not region:
         region = auth_config.get('region')
 
+    # Prompt for token and region
     token = click.prompt(f'Please enter your token. ({"*" * 6 + token[6:]})', type=str, show_default=False,
                         default=profile_auth.get('token'))
     region = click.prompt(f'Choose your Region ({region})', show_choices=True, default=region, show_default=True,
                           type=str)
-
+    # Construct the host
     host_ = host or f"https://{(region + '.') if region and region != 'us-east-2' else ''}console.cli.io"
-
+    # Make the profile yaml
     auth_config[profile] = {"token": token,
                             "host": host_,
                             "region": region}
 
+    # Write it to a file
     with open(auth_path, 'w') as af:
-        yaml.dump(auth_config, stream=af)
+        yaml.safe_dump(auth_config, stream=af)
 
     click.echo(f'Thank you for entering auth credentials. Please check your profile at: {auth_path.name}')
 
