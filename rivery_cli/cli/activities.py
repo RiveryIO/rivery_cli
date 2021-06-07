68931184de5c2c6e30c044698d50c215b121ae62
Gilad Levy <giladlevy7@gmail.com>
2021-06-07 23:03:13 +0300

    few improvements

--- rivery_cli/cli/activities.py
+++ rivery_cli/cli/activities.py
@@ -29,6 +29,8 @@ def fetch_run_logs(session, run_id):
 @runs.command("fetch")
 @click.option("--runId", required=True, type=str,
               help="""The run id that will be used to filter the logs.""")
+@click.option("--filePath", required=False, type=str,
+              help="""The file that the logs should be saved to.""")
 @click.pass_obj
 @decorators.error_decorator
 def download_run_logs(ctx, **kwargs):
@@ -36,7 +38,7 @@ def download_run_logs(ctx, **kwargs):
     profile_name = ctx.get('PROFILE')
     run_id = kwargs.get('runid')
 
-    click.echo(f'Starting to download logs for run id {run_id} in profile {profile_name}')
+    click.echo(f'Starting to download logs for run id {run_id} in profile: {profile_name}')
     rivery_client = client.Client(name=profile_name)
     session = rivery_client.session
 
@@ -48,9 +50,16 @@ def download_run_logs(ctx, **kwargs):
         spinner.stop()
 
     click.echo(
-        f'Run ID {run_id} logs fetched successfully. '
-        f'{resp.content}'
+        f'Run ID {run_id} logs fetched successfully.'
     )
+    file_path = kwargs.get('filepath')
+    if file_path:
+        click.echo(f'Saving logs to file: {file_path}')
+        f = open(file_path, "a")
+        f.write(str(resp.content))
+        f.close()
+    else:
+        click.echo(f'Logs content: {resp.content}')
 
 
 if __name__ == '__main__':
